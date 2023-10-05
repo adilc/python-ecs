05a0a8d6cb4efe0f2e95dfb723d6206710d9db19
Adil Can <adil_can@hotmail.com>
2023-10-05 14:13:04 -0700

    Update create.py

--- create.py
+++ create.py
@@ -11,10 +11,6 @@ def hello():
 
 # noncompliant example
 
-def read_file_noncompliant(filename):
-    file = open(filename, 'r')
-    # Noncompliant: method returns without properly closing the file.
-    return file.readlines()
 
 
 def create_service():
